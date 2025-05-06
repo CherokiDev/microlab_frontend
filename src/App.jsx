e7ef5a08e8f8a7273afcc284227e2aee652a709f
CherokiDev <miguelangelmorato84@gmail.com>
2025-05-06 19:13:00 +0200

    refactor: remove unnecessary comment about cleaning up MQTT connection

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,6 @@ function App() {
         console.error("Error en la conexión MQTT:", err);
       });
 
-      // Limpiar conexión al desmontar el componente
       return () => {
         client.end();
       };
